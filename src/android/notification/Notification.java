bb58410016b5a39e6d55c8d7e3b8d519e8d2b4a3
Angus McLean <angus.mclean@mail.mcgill.ca>
2017-01-26 00:52:35 -0500

    trying changes

--- src/android/notification/Notification.java
+++ src/android/notification/Notification.java
@@ -198,7 +198,7 @@ public class Notification {
 				builder.extend(new WearableExtender().addAction(action));
 
 				// NotificationCompat.Notification notification = builder.build();
-				getNotMgr().notify(1, builder.build();
+				getNotMgr().notify(1, builder.build());
 
 			} else {
 				pi = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
